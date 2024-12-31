2779f5c29adcecc0fc64eeb137860eb2bdf65d34
josh fernandes <joshfernandes@mac.com>
2024-12-31 10:28:05 -0800

    added more whitespace

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ setup_dir = os.path.dirname(os.path.abspath(__file__))
 requirements_file = os.path.join(setup_dir, "requirements.txt")
 version_file = os.path.join(setup_dir, "version.txt")
 
+
 # Helper functions to read text files.
 def get_requirements():
     with open(requirements_file, "r", encoding="utf-8") as f:
         return f.read().splitlines()
+
 
 def get_version():
     with open(version_file, "r", encoding="utf-8") as f:
@@ -17,6 +19,7 @@ def get_version():
             if line.startswith("__version__"):
                 return line.split("=")[1].strip().strip('"').strip("'")
         return RuntimeError("Error: Version number not found.")
+
 
 setup(
     name="qquest",
