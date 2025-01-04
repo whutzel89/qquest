34a8eb656ccb726bb3f40ad26144adaf3f6ba5f1
josh fernandes <joshfernandes@mac.com>
2025-01-03 21:47:10 -0800

    add example scripts

---
+++ scripts/activate_poetry_env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Note: The run a single command inside the virtual environment (such as pytest)
+# refer to the following example code.
+#           ex. 
+#               > poetry run pytest
+poetry shell
